99a3d3da14715b2f31d21cf88396296895dda8b1
ThankGod88 <enemuothankgod@yahoo.com>
2022-04-27 18:41:58 -0700

    Task 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,8 +2,8 @@
 /**
  *_memset -   fills memory with a constant byte
  *@s: pointer block of memory to fill
- *@b: value to set
  *@n: bytes of the memory
+ *@b: value to set
  *Return: 
  */
 
@@ -12,6 +12,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
+	/*Delacring FOR*/
 	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b; /*add 1 position s*/
